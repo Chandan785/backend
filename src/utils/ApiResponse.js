fa032e7c173a08c93509b424ed7ac5bf515790c7
chandan kumar <chandankumar700451@gamil.com>
2025-06-09 12:28:49 +0530

    utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statuscode ,data ,massage = 'Success', success = true) {
+        this.statusCode = statuscode < 400;
+        this.data = data;
+        this.message = massage;
+        this.success = success;
+    }
+}
---
+++ src/utils/asyncHandeler.js
@@ -0,0 +1,32 @@
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+  return Promise.resolve(requestHandler(req, res, next))
+    .catch(next);
+  }
+}
+
+
+
+
+
+
+
+export {asyncHandler}
+
+//const asyncHandler = () => {}
+//  const asyncHandler = (func) =>()=> {}
+//  const asyncHandler = (func) =>(aync)=> {}
+
+/*
+    
+const asyncHandler = (func) => async (req, res, next) => {
+  try {
+    await func(req, res, next);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+} 
+*/
+
