6909e780187eddeac679364070a3358c17d872e6
Chandan kumar <chandankumar700451@gmail.com>
2025-06-12 12:54:40 +0530

    complete subscription.model.js

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose ,{model, Schema} from "mongoose";
+const subscriptionSchema = new mongoose.Schema({
+  subcriber:{
+    type :Schema.Types.ObjectId,//one who is subscribing
+    ref: "User",
+    
+  },
+  channel:{
+    type: Schema.Types.ObjectId,//one whom subscriber is subscribing to 
+    ref: "User",
+  }
+}
+, {
+  timestamps: true,
+});
+
+export const Subscription = model("Subscription", subscriptionSchema);
